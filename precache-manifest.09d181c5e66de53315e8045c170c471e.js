2aa44fd02ef4c71dc334a0af25b38aeea481a6b6
grey4uk <grey4uk14@gmail.com>
2020-03-09 20:38:40 +0200

    Updates

---
+++ precache-manifest.09d181c5e66de53315e8045c170c471e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b361b9e3161d99ef94e7ae221cb346c",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "ea9002f2ae31375af05a",
+    "url": "/goit-react-hw-02-feedback/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a83c19a6a28ad17470e5",
+    "url": "/goit-react-hw-02-feedback/static/js/2.029a0841.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.029a0841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9002f2ae31375af05a",
+    "url": "/goit-react-hw-02-feedback/static/js/main.df638b7e.chunk.js"
+  },
+  {
+    "revision": "8f295445564cac0e313f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.23cf4f98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goit-react-hw-02-feedback/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-phonebook/precache-manifest.a3d8878239bb68285b659714d39abe83.js"
+  "/goit-react-hw-02-feedback/precache-manifest.09d181c5e66de53315e8045c170c471e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-02-phonebook/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-02-feedback/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
